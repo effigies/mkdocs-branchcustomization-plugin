d79dfa05227135a3263bf629e20087ef9f17bc65
Christopher J. Markiewicz <markiewicz@stanford.edu>
2019-12-13 10:55:25 -0500

    ENH: Determine best name for commit

--- mkdocs_branchcustomization_plugin/plugin.py
+++ mkdocs_branchcustomization_plugin/plugin.py
@@ -5,6 +5,21 @@ from mkdocs.config.config_options import Type
 import git
 
 
+def get_best_name(repo):
+    head = repo.head
+    # We're on a branch, just go with it
+    if not head.is_detached:
+        return repo.active_branch.name
+    # This commit is the current master
+    if repo.heads.master.commit == head.commit:
+        return 'master'
+    # Prioritize tags and then branches in lexical order
+    for ref in repo.tags + repo.heads:
+        if ref.commit == head.commit:
+            return ref.name
+    return head.commit.hexsha
+
+
 class BranchPlugin(BasePlugin):
     config_scheme = (
         ('update_config', Type(list)),
@@ -15,7 +30,8 @@ class BranchPlugin(BasePlugin):
         self.repo = git.Repo('.', search_parent_directories=True)
 
     def on_config(self, config):
-        branch_name = self.repo.active_branch.name
+        branch_name = get_best_name(self.repo)
+        log.debug(f"Detected HEAD name: {branch_name}")
         if self.config['update_config']:
             for rule in self.config['update_config']:
                 match = rule.pop('branch', None)
